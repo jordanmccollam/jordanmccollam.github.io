3b2e3c2c349beee4acf55a177535763ffa1dc0ad
Jordan McCollam <jordy.mccollam@gmail.com>
2021-04-22 17:43:31 -0400

    Made contact form

--- src/screens/Contact/Contact.js
+++ src/screens/Contact/Contact.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types'
 import classnames from "classnames"
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import { Card } from '../../components';
+import { FaPaperPlane } from 'react-icons/fa';
 
 import './_contact.scss';
 
@@ -19,8 +20,17 @@ const Contact = (props) => {
         <h1 className="tracking-in-expand">CONTACT</h1>
 
         <Row className="content swing-top-fwd justify-content-center">
-          <Col lg={10}>
-
+          <Col lg={8}>
+            <h5 className="text-white">Let's talk!</h5>
+            <form action="https://formspree.io/f/jordy.mccollam@gmail.com" method="POST" >
+              <Form.Control placeholder="Email" type="email" name="_replyto" />
+              <Form.Control className="mt-2" placeholder="Subject" name="subject" />
+              <Form.Control className="mt-2" as="textarea" rows={5} placeholder="Message" name="message" />
+              <div className="d-flex justify-content-between mt-2">
+                <Button type="submit">Send <FaPaperPlane/></Button>
+                <div className="text-white">Or email directly at jordy.mccollam@gmail.com</div>
+              </div>
+            </form>
           </Col>
         </Row>
       
